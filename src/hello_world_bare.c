a9cde72dc8be51df5ce06618e43e8af298c44b08
Guenther Beulen <guenther.beulen@web.de>
2025-01-24 12:36:11 +0100

    Add receiving at both UARTs!

--- src/hello_world_bare.c
+++ src/hello_world_bare.c
@@ -49,6 +49,8 @@ static void CLK_init(void)
 
 static void EUSART1_init(void)
 {
+    /* Receive Enable */
+    U1CON0bits.RXEN = 1;
     /* Transmit Enable */
     U1CON0bits.TXEN = 1;
     /* High Baud Rate Select */
@@ -67,7 +69,9 @@ static void EUSART1_init(void)
 
 static void EUSART2_init(void)
 {
-    /* Transmit Enable */
+    /* Receive Enable */
+    U2CON0bits.RXEN = 1;
+     /* Transmit Enable */
     U2CON0bits.TXEN = 1;
     /* High Baud Rate Select */
     U2CON0bits.BRGS = 1;
@@ -80,7 +84,7 @@ static void EUSART2_init(void)
     U2BRGH = 0;
 
     /* Serial Port Enable */
-    U2CON1bits.ON = 1;
+    U2CON1bits.On = 1;
 }
 
 static void PPS_init(void) 
@@ -103,8 +107,16 @@ static void PORT_init(void)
     /* Configure RC6 as output. */
     TRISCbits.TRISC6 = 0;
 
+    /* Configure RC7 as input. */
+    TRISCbits.TRISC7 = 1;
+    ANSELCbits.ANSELC7 = 0;
+
     /* Configure RC0 as output. */
     TRISCbits.TRISC0 = 0;
+
+    /* Configure RC1 as input. */
+    TRISCbits.TRISC1 = 1;
+    ANSELCbits.ANSELC1 = 0;
 }
 
 static void EUSART1_write(uint8_t txData)
@@ -127,23 +139,73 @@ static void EUSART2_write(uint8_t txData)
     U2TXB = txData;
 }
 
+uint8_t EUSART1_read(void)
+{
+    while(0 == PIR3bits.U1RXIF)
+    {
+        ;
+    }
+
+    return U1RXB;
+}
+
+uint8_t EUSART2_read(void)
+{
+    while(0 == PIR7bits.U2RXIF)
+    {
+        ;
+    }
+
+    return U2RXB;
+}
+
+void putch1(char txData)
+{
+    EUSART1_write(txData);
+}
+
+void putch2(char txData)
+{
+    EUSART2_write(txData);
+}
+
+char getch1(void)
+{
+    return EUSART1_read();
+}
+
+char getch2(void)
+{
+    return EUSART2_read();
+}
+
+
+
 void main(void)
 {
     char msg[] = "Hello World\r\n";
     char msg2[] = "Hallo Welt\r\n";
+
+    char c1 = ' ';
+    char c2 = ' ';
 
     CLK_init();
     EUSART1_init();
     EUSART2_init();  
     PPS_init();   
     PORT_init();
+
+    for(uint8_t i = 0; i < strlen(msg); i++)
+    {
+        EUSART1_write(msg2[i]);
+        EUSART2_write(msg[i]);
+    }
     
     while(1) 
     {
-        for(uint8_t i = 0; i < strlen(msg); i++) 
-        {
-            EUSART1_write(msg2[i]);
-            EUSART2_write(msg[i]);
-        }
+        c1 = getch1();
+        putch2(c1);
+        c2 = getch2();
+        putch1(c2);
     }
 }
