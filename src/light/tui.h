fc422c4b6a1640ac142062a8811aa16cb7e38b22
Guenther Beulen <guenther.beulen@web.de>
2025-07-27 16:59:16 +0200

    Initial commit of Terminal User Interface files

---
+++ src/light/tui.h
@@ -0,0 +1,14 @@
+///
+// @file
+///
+
+#ifndef DALIINTERFACE_LIGHT_TUI
+#define DALIINTERFACE_LIGHT_TUI
+
+const char* CursorBackward = {""};
+const char* CursorDown = {""};
+const char* CursorForward = {""};
+const char* CursorPosition = {""};
+const char* CursorUp = {""};
+
+#endif // DALIINTERFACE_LIGHT_TUI
