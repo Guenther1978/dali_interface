ceb265f716235c860f8101fc4eaf8372f44e4185
Guenther Beulen <guenther.beulen@web.de>
2025-08-03 17:57:21 +0200

    Add funcions for moving the cursor!

--- src/light/tui.c
+++ src/light/tui.c
@@ -1,16 +1,62 @@
 #include "tui.h"
-#i#include "../uart/uart1.h"
+#include "../uart/uart1.h"
 
 void DrawCorner(void) {
-  ('+');
+  UART1_write('+');
 }
 
 void DrawHorizontalLineLeft2Right(uint8_t lenght) {
   UART1_write('-');
   for (int i = length - 1; i > 0; i--) {
-    for(uint8_t i = 0; i < strlen(kCursorForward); i++) {
-      EUSART1_write(msg[i]);
-    }
+    MoveCursorForward();
     UART1_write('-');
+  }
+}
+
+void DrawHorizontalLineRight2Left(uint8_t lenght) {
+  UART1_write('-');
+  for (int i = length - 1; i > 0; i--) {
+    MoveCursorBackward();
+    UART1_write('-');
+  }
+}
+
+void DrawVerticalLineBottom2Top(uint8_t lenght) {
+  UART1_write('|');
+  for (int i = length - 1; i > 0; i--) {
+    MoveCursorUp();
+    UART1_write('|');
+  }
+}
+
+void DrawVerticalLineTop2Bottom(uint8_t lenght) {
+  UART1_write('|');
+  for (int i = length - 1; i > 0; i--) {
+    MoveDursorDown();
+    UART1_write('|');
+  }
+}
+
+void MoveCursorDown(void) {
+  for(uint8_t i = 0; i < strlen(kCursorDown); i++) {
+      UART1_write(msg[i]);
+  }
+}
+
+void MoveCursorForward(void) {
+  for(uint8_t i = 0; i < strlen(kCursorForward); i++) {
+      UART1_write(msg[i]);
+  }
+}
+
+void MoveCursorBackward(void) {
+  for(uint8_t i = 0; i < strlen(kCursorBackward); i++) {
+      UART1_write(msg[i]);
+  }
+}
+
+void MoveCursorUp(void) {
+  for(uint8_t i = 0; i < strlen(kCursorUp); i++) {
+      UART1_write(msg[i]);
   }
 }
--- src/light/tui.h
+++ src/light/tui.h
@@ -17,8 +17,12 @@ void DrawCorner(void);
 void DrawHorizontalLineFromLeft2Right(uint8_t);
 void DrawHorizontalLineFromRight2Left(uint8_t);
 void DrawRectangle(uint8_t, uint8_t);
-void DrawVerticalLineFromDown2Up(uint8_t);
-void DrawVerticalLineFromUp2Down(uint8_t);
+void DrawVerticalLineFromBottom2Top(uint8_t);
+void DrawVerticalLineFromTop2Bottom(uint8_t);
+void MoveCursorDown(void);
+void MoveCursorForward(void);
+void MoveCursorBackward(void);
+void MoveCursorUp(void);
 void SetCursor(uint8_t, uint8_t);
 
 #endif // DALIINTERFACE_LIGHT_TUI
