2d9fe4ba2ccae99f6a271a3d68cd2880cf75a26f
Guenther Beulen <guenther.beulen@web.de>
2025-10-20 21:43:40 +0200

    Improve Arduino example!

--- arduino_vt100/arduino_vt100.ino
+++ arduino_vt100/arduino_vt100.ino
@@ -1,17 +1,78 @@
 #include <Arduino.h>
 
-#include "tui.h"
-
 unsigned char buffer[16];
-const uint8_t buffersize = 16;
+
+const uint8_t kBufferSize = 16;
+const uint8_t kLineTop = 1;
+const uint8_t kLineBottom = 24;
+const uint8_t kColumnLeft = 1;
+const uint8_t kColumnRight = 80;
+const char* kCursorHome = {"\27[H"};
+const char* kClearScreen = {"\27[2J"};
+
+void MoveCursorToPosition(uint8_t line, uint8_t column, unsigned char* puffer) {
+  //  puffer[0] = 27;
+  //  puffer[1] = '[';
+  //  sprintf(puffer+2, "%d;%dH",y,x);
+  // the first specifying the line position and the second specifying the column position
+  sprintf(puffer, "%c[%d;%dH", 27, line, column);
+}
+
+void ClearScreen(unsigned char* puffer) {
+  sprintf(puffer, "%c[2J", 27);
+}
+
+
+void ClearBuffer(unsigned char * buffer, unsigned char size_of_buffer) {
+  for (int i = 0; i < size_of_buffer; i ++) {
+    buffer[i] = ' ';
+  }
+}
 
 void setup() {
   Serial.begin(9600);
   while (!Serial);
 
-  Serial.print(kCursorHome);
+  ClearScreen(buffer);
+  Serial.write(buffer, kBufferSize);
 
+  for (uint8_t i = kLineTop + 1; i < kLineBottom; i++) {
+    MoveCursorToPosition(i, kColumnLeft, buffer);
+    Serial.write(buffer, kBufferSize);
+    Serial.write('|');
+    MoveCursorToPosition(i, kColumnRight, buffer);
+    Serial.write(buffer, kBufferSize);
+    Serial.write('|');
+  }
 
+  delay(1000);
+
+  for (uint8_t i = kColumnLeft + 1; i < kColumnRight; i++) {
+    MoveCursorToPosition(kLineTop, i, buffer);
+    Serial.write(buffer, kBufferSize);
+    Serial.write('-');
+    MoveCursorToPosition(kLineBottom, i, buffer);
+    Serial.write(buffer, kBufferSize);
+    Serial.write('-');
+  }
+
+  delay(1000);
+
+  MoveCursorToPosition(kLineTop, kColumnLeft, buffer);
+  Serial.write(buffer, kBufferSize);
+  Serial.write('+');
+
+  MoveCursorToPosition(kLineBottom, kColumnLeft, buffer);
+  Serial.write(buffer, kBufferSize);
+  Serial.write('+');
+
+  MoveCursorToPosition(kLineTop, kColumnRight, buffer);
+  Serial.write(buffer, kBufferSize);
+  Serial.write('+');
+
+  MoveCursorToPosition(kLineBottom, kColumnRight, buffer);
+  Serial.write(buffer, kBufferSize);
+  Serial.write('+');
 }
 
 void loop() {
@@ -21,16 +82,9 @@ void loop() {
 
   if (Serial.available() > 0) {
     Serial.read();
-    MoveCursorToPosition(11, 22, buffer);
-    Serial.write(buffer, 16);
-
-    for (int i = 0; i < strlen(buffer); i++) {
-      Serial.print((int)buffer[i]); // Print the ASCII value of each character
-      Serial.print(" ");
-    }
+    MoveCursorToPosition(3, 20, buffer);
+    Serial.write(buffer, kBufferSize);
 
     Serial.println("Hallo Welt");
-
   }
-
 }
