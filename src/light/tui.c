d1dadf1a61dbea3201e080d324a81d3588b07b09
Guenther Beulen <guenther.beulen@web.de>
2025-11-16 19:10:44 +0100

    Add TuiFsm to library!

--- src/light/tui.c
+++ src/light/tui.c
@@ -1,68 +1,242 @@
+#include <stdio.h>
+
 #include "tui.h"
-#include "../uart/uart1.h"
+//#include "mcc_generated_files/uart/uart1.h"
+//#include "mcc_generated_files/system/system.h"
 
-void DrawCorner(void) {
-  UART1_write('+');
+
+void InitTuiFsm(TuiFsm* tui_fsm) {
+    // Draw Rectangle
+  ClearScreen(tui_fsm);
+  for(uint8_t i = 0; i < strlen(tui_fsm->buffer_command); i++)
+    {
+        UART1_Write(tui_fsm->buffer_command[i]);
+    }
+
+  MoveCursorHome(tui_fsm);
+  for(uint8_t i = 0; i < strlen(tui_fsm->buffer_command); i++)
+    {
+        UART1_Write(tui_fsm->buffer_command[i]);
+    }
+
+  UART1_Write('+');
+  for(uint8_t i = kColumnRight - 2; i > 0; i--)
+    {
+        UART1_Write('-');
+    }
+  UART1_Write('+');
+  UART1_Write('\r');
+  UART1_Write('\n');
+
+  for (uint8_t j = kLineBottom - 2; j > 0; j--)
+  {
+    UART1_Write('|');
+    for(uint8_t i = kColumnRight - 2; i > 0; i++)
+      {
+          UART1_Write(' ');
+      }
+    UART1_Write('|');
+    UART1_Write('\r');
+    UART1_Write('\n');
+  }
+
+   UART1_Write('+');
+  for(uint8_t i = kColumnRight - 2; i > 0; i--)
+    {
+        UART1_Write('-');
+    }
+
+  MoveCursorToPosition(kLineCaption, kColumnValue, tui_fsm);
+  for(uint8_t i = 0; i < strlen(tui_fsm->buffer_command); i++)
+    {
+        UART1_Write(tui_fsm->buffer_command[i]);
+    }
+
+  for(uint8_t i = 0; i < strlen(kCaption); i++)
+    {
+        UART1_Write(kCaption[i]);
+    }
+
+  MoveCursorToPosition(kLineCaption + 1, kColumnValue - 1, tui_fsm);
+  for(uint8_t i = 0; i < strlen(tui_fsm->buffer_command); i++)
+    {
+        UART1_Write(tui_fsm->buffer_command[i]);
+    }
+
+  for (uint8_t i = kColumnValue - 1; i < 35  ; i++) {
+      UART1_Write('-');
+  }
+
+
+  // Add Labels
+  MoveCursorToPosition(kLineNumber, kColumnLabel, tui_fsm);
+  for(uint8_t i = 0; i < strlen(tui_fsm->buffer_command); i++)
+    {
+        UART1_Write(tui_fsm->buffer_command[i]);
+    }
+  for(uint8_t i = 0; i < strlen(kNumber); i++)
+    {
+        UART1_Write(kNumber[i]);
+    }
+
+  MoveCursorToPosition(kLineValue, kColumnLabel, tui_fsm);
+  for(uint8_t i = 0; i < strlen(tui_fsm->buffer_command); i++)
+    {
+        UART1_Write(tui_fsm->buffer_command[i]);
+    }
+  for(uint8_t i = 0; i < strlen(kValue); i++)
+    {
+        UART1_Write(kValue[i]);
+    }
+
+  MoveCursorToPosition(kLineReception, kColumnLabel, tui_fsm);
+  for(uint8_t i = 0; i < strlen(tui_fsm->buffer_command); i++)
+    {
+        UART1_Write(tui_fsm->buffer_command[i]);
+    }
+  for(uint8_t i = 0; i < strlen(kReception); i++)
+    {
+        UART1_Write(kReception[i]);
+    }
+
+  MoveCursorToPosition(kLineMessage, kColumnLabel, tui_fsm);
+  for(uint8_t i = 0; i < strlen(tui_fsm->buffer_command); i++)
+    {
+        UART1_Write(tui_fsm->buffer_command[i]);
+    }
+  for(uint8_t i = 0; i < strlen(kMessage); i++)
+    {
+        UART1_Write(kMessage[i]);
+    }
+
+  InitMessage(tui_fsm);
 }
 
-void DrawHorizontalLineLeft2Right(uint8_t lenght) {
-  UART1_write('-');
-  for (int i = length - 1; i > 0; i--) {
-    MoveCursorForward();
-    UART1_write('-');
+void ShowDaliAnswer(TuiFsm* tui_fsm, uint8_t answer) {
+  MoveCursorToPosition(kLineReception, kColumnValue, tui_fsm);
+  for(uint8_t i = 0; i < strlen(tui_fsm->buffer_command); i++)
+    {
+        UART1_Write(tui_fsm->buffer_command[i]);
+    }
+  UART1_Write(answer);
+  tui_fsm->state = CHOOSE_ACTION;
+}
+
+void InitMessage(TuiFsm* tui_fsm) {
+  MoveCursorToPosition(kLineMessage, kColumnValue, tui_fsm);
+  Serial.write(tui_fsm->buffer_command, kBufferSizeCommand);
+  Serial.print("Write number (n), write value (v) or send data (s)");
+  tui_fsm->state = CHOOSE_ACTION;
+}
+
+void InitReadNumber(TuiFsm* tui_fsm) {
+  tui_fsm->state = READ_NUMBER;
+  tui_fsm->number = 0;
+  for (uint8_t i = 0; i < 3; i++) {
+    tui_fsm->buffer_number[i] = '0';
+  }
+  tui_fsm->buffer_number[3] = 0;
+  MoveCursorToPosition(kLineMessage, kColumnValue, tui_fsm);
+  Serial.write(tui_fsm->buffer_command, kBufferSizeCommand);
+  Serial.print("Geben Sie die Nummer der Lampe ein!");
+  MoveCursorToPosition(kLineNumber, kColumnValue, tui_fsm);
+  Serial.write(tui_fsm->buffer_command, kBufferSizeCommand);
+}
+
+void InitReadValue(TuiFsm* tui_fsm) {
+  tui_fsm->state = READ_VALUE;
+  tui_fsm->value = 0;
+  for (uint8_t i = 0; i < 3; i++) {
+    tui_fsm->buffer_value[i] = '0';
+  }
+  tui_fsm->buffer_value[3] = 0;
+  MoveCursorToPosition(kLineMessage, kColumnValue, tui_fsm);
+  Serial.write(tui_fsm->buffer_command, kBufferSizeCommand);
+  Serial.print("Geben Sie die Intensitaet oder den Befehl ein!");
+  MoveCursorToPosition(kLineValue, kColumnValue, tui_fsm);
+  Serial.write(tui_fsm->buffer_command, kBufferSizeCommand);
+}
+
+void MoveCursorToPosition(uint8_t line, uint8_t column, TuiFsm* puffer) {
+  sprintf(puffer->buffer_command, "%c[%d;%dH", 27, line, column);
+}
+
+void ClearScreen(TuiFsm* puffer) {
+  sprintf(puffer->buffer_command, "%c[2J", 27);
+}
+
+void ClearLine(TuiFsm* puffer) {
+  sprintf(puffer->buffer_command, "%c[0K", 27);
+}
+
+void MoveCursorHome(TuiFsm* puffer) {
+  sprintf(puffer->buffer_command, "%c[H", 27);
+}
+
+void ClearBuffer(unsigned char * buffer, unsigned char size_of_buffer) {
+  for (int i = 0; i < size_of_buffer; i ++) {
+    buffer[i] = '0';
   }
 }
 
-void DrawHorizontalLineRight2Left(uint8_t lenght) {
-  UART1_write('-');
-  for (int i = length - 1; i > 0; i--) {
-    MoveCursorBackward();
-    UART1_write('-');
+void HandleChar(char input, TuiFsm* tui_fsm) {
+  if ((input >= '0') && (input <= '9')) {
+    switch (tui_fsm->state) {
+      case READ_NUMBER:
+        tui_fsm->buffer_number[0] = tui_fsm->buffer_number[1];
+        tui_fsm->buffer_number[1] = tui_fsm->buffer_number[2];
+        tui_fsm->buffer_number[2] = input;
+        MoveCursorToPosition(kLineNumber, kColumnValue, tui_fsm);
+        Serial.write(tui_fsm->buffer_command, kBufferSizeCommand);
+        for (uint8_t i = 0; i < 3; i++) {
+          Serial.write(tui_fsm->buffer_number[i]);
+        }
+        tui_fsm->number = atoi(tui_fsm->buffer_number);
+        break;
+      case READ_VALUE:
+        tui_fsm->buffer_value[0] = tui_fsm->buffer_value[1];
+        tui_fsm->buffer_value[1] = tui_fsm->buffer_value[2];
+        tui_fsm->buffer_value[2] = input;
+        MoveCursorToPosition(kLineValue, kColumnValue, tui_fsm);
+        Serial.write(tui_fsm->buffer_command, kBufferSizeCommand);
+        for (uint8_t i = 0; i < 3; i++) {
+          Serial.write(tui_fsm->buffer_value[i]);
+        }
+        tui_fsm->value = atoi(tui_fsm->buffer_value);
+        break;
+      default:
+        break;
+    }
   }
-}
-
-void DrawVerticalLineBottom2Top(uint8_t lenght) {
-  UART1_write('|');
-  for (int i = length - 1; i > 0; i--) {
-    MoveCursorUp();
-    UART1_write('|');
+  if ((input == 'N') || (input == 'n')) {
+    InitReadNumber(tui_fsm);
   }
-}
-
-void DrawVerticalLineTop2Bottom(uint8_t lenght) {
-  UART1_write('|');
-  for (int i = length - 1; i > 0; i--) {
-    MoveDursorDown();
-    UART1_write('|');
+  if ((input == 'V') || (input == 'v')) {
+    InitReadValue(tui_fsm);
   }
-}
+  if ((input == 'S') || (input == 's')) {
+    MoveCursorToPosition(kLineMessage, kColumnValue, tui_fsm);
+    Serial.write(tui_fsm->buffer_command, kBufferSizeCommand);
+    Serial.print(F("Sending bytes: "));
+    Serial.print(tui_fsm->number);
+    Serial.print(F(" "));
+    Serial.write(tui_fsm->number);
+    Serial.print(F(", "));
+    Serial.print(tui_fsm->value);
+    Serial.print(F(" "));
+    Serial.write(tui_fsm->value);
+    Serial.print(F(" "));
 
-void MoveCursorDown(void) {
-  for(uint8_t i = 0; i < strlen(kCursorDown); i++) {
-      UART1_write(msg[i]);
+    DaliBus.write(tui_fsm->number);
+    DaliBus.write(tui_fsm->value);
+
+    ClearLine(tui_fsm);
+    Serial.write(tui_fsm->buffer_command, kBufferSizeCommand);
+    MoveCursorToPosition(kLineMessage, kColumnRight, tui_fsm);
+    Serial.write(tui_fsm->buffer_command, kBufferSizeCommand);
+    Serial.print("|");
   }
-}
-
-void MoveCursorForward(void) {
-  for(uint8_t i = 0; i < strlen(kCursorForward); i++) {
-      UART1_write(msg[i]);
+  if (input == '/r') {
+    InitMessage(tui_fsm);
   }
-}
-
-void MoveCursorBackward(void) {
-  for(uint8_t i = 0; i < strlen(kCursorBackward); i++) {
-      UART1_write(msg[i]);
-  }
-}
-
-void MoveCursorUp(void) {
-  for(uint8_t i = 0; i < strlen(kCursorUp); i++) {
-      UART1_write(msg[i]);
-  }
-}
-
-void PrintBottomOrUpperLine(uint8_t) {
-}
-
-void PrintMiddleLine(uint8_t);
 }
--- src/light/tui.h
+++ src/light/tui.h
@@ -5,40 +5,50 @@
 #ifndef DALIINTERFACE_LIGHT_TUI
 #define DALIINTERFACE_LIGHT_TUI
 
-#include <stdbool.h>
-#include <stdint.h>
+//#include <xc.h>
+//#include <stdint.h>
+//#include <stdio.h>
+#include <string.h>
+#include "../mcc_generated_files/uart/uart1.h"
+//#include "../mcc_generated_files/uart/uart2.h"
 
-const char* kCursorBackward = {"\033["};
-const char* kCursorDown = {"\033["};
-const char* kCursorForward = {"\033["};
-const char* kCursorHome = {"\033[H");
-const char* kCursorPosition = {"\033["};
-const char* kCursorUp = {"\033["};
-const uint8_t kIndexFirstColumn = 1;
-const uint8_t kIndexFirstLine = 1;
-const uint8_t kNumberOfColumns = 80;
-const uint8_t kNumberOfLines = 24;
+const char* kCaption = {"USB-Dali-Interface"};
+const char* kNumber = {"Number"};
+const char* kValue = {"Value"};
+const char* kReception = {"Reception"};
+const char* kMessage = {"Message"};
 
-typedef struct Point {
-  uint8_t column;
-  uint8_t line;
-};
+const char* kCursorBackward = {"\0x1B"};
+const char* kCursorDown = {"\0x18"};
+const char* kCursorForward = {"\0x18"};
+const char* kCursorPosition = {"\0x18"};
+const char* kCursorUp = {"\0x18"};
 
-typedef struct Line {
-  struct Point;
-  uint8_t length;
-};
+const uint8_t kBufferSizeCommand = 16;
+const uint8_t kBufferSizeNumber = 4;
+const uint8_t kLineTop = 1;
+const uint8_t kLineBottom = 24;
+const uint8_t kColumnLeft = 1;
+const uint8_t kColumnRight = 80;
 
-typedef struct Rectangle {
-  struct Point;
-  uint8_t colums;
-  uint8_t lines;
-};
+const char* kCursorHome = {"\27[H"};
+const char* kClearScreen = {"\27[2J"};
+
+const uint8_t kColumnLabel = 4;
+const uint8_t kColumnValue = 16;
+const uint8_t kLineCaption = 3;
+const uint8_t kLineNumber = 7;
+const uint8_t kLineValue = 10;
+const uint8_t kLineReception = 13;
+const uint8_t kLineMessage = 20;
+
+void DrawHorizontalLine(void);
+void DrawVerticalLine(void);
+void MoveCursorHome(void);
 
 void DrawCorner(void);
 void DrawHorizontalLineFromLeft2Right(uint8_t);
 void DrawHorizontalLineFromRight2Left(uint8_t);
-void DrawBlankRectangle();
 void DrawRectangle(uint8_t, uint8_t);
 void DrawVerticalLineFromBottom2Top(uint8_t);
 void DrawVerticalLineFromTop2Bottom(uint8_t);
@@ -46,8 +56,45 @@ void MoveCursorDown(void);
 void MoveCursorForward(void);
 void MoveCursorBackward(void);
 void MoveCursorUp(void);
-void PrintBottomOrUpperLine(uint8_t);
-void PrintMiddleLine(uint8_t);
 void SetCursor(uint8_t, uint8_t);
+
+typedef enum {
+  CHOOSE_ACTION,
+  READ_NUMBER,
+  READ_VALUE,
+  RECEIVE,
+  SEND
+} State;
+
+typedef struct TuiFsm {
+  State state;
+  unsigned char buffer_number[3];
+  unsigned char buffer_value[3];
+  unsigned char buffer_command[16];
+  uint8_t number;
+  uint8_t value;
+};
+
+TuiFsm tui_fsm;
+
+void InitTuiFsm(TuiFsm* tui_fsm);
+
+void ShowDaliAnswer(TuiFsm* tui_fsm, uint8_t answer);
+
+void InitMessage(TuiFsm* tui_fsm);
+
+void InitReadNumber(TuiFsm* tui_fsm);
+
+void InitReadValue(TuiFsm* tui_fsm);
+
+void MoveCursorToPosition(uint8_t line, uint8_t column, TuiFsm* puffer);
+
+void ClearScreen(TuiFsm* puffer);
+
+void ClearLine(TuiFsm* puffer);
+
+void ClearBuffer(unsigned char * buffer, unsigned char size_of_buffer);
+
+void HandleChar(char input, TuiFsm* tui_fsm);
 
 #endif // DALIINTERFACE_LIGHT_TUI
