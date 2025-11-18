73e83a5a06218f5d32575a46653c21bbe22abcba
Guenther Beulen <guenther.beulen@web.de>
2025-11-18 22:21:38 +0100

    Add functionality!

--- src/light/tui.h
+++ src/light/tui.h
@@ -5,40 +5,55 @@
 #ifndef DALIINTERFACE_LIGHT_TUI
 #define DALIINTERFACE_LIGHT_TUI
 
-#include <stdbool.h>
-#include <stdint.h>
+//#include <xc.h>
+//#include <stdint.h>
+//#include <stdio.h>
+#include <string.h>
+#include "../mcc_generated_files/uart/uart1.h"
+//#include "../mcc_generated_files/uart/uart2.h"
 
-const char* kCursorBackward = {"\033["};
-const char* kCursorDown = {"\033["};
-const char* kCursorForward = {"\033["};
-const char* kCursorHome = {"\033[H");
-const char* kCursorPosition = {"\033["};
-const char* kCursorUp = {"\033["};
-const uint8_t kIndexFirstColumn = 1;
-const uint8_t kIndexFirstLine = 1;
-const uint8_t kNumberOfColumns = 80;
-const uint8_t kNumberOfLines = 24;
+const char* kCaption = {"USB-Dali-Interface"};
+const char* kNumber = {"Number"};
+const char* kValue = {"Value"};
+const char* kReception = {"Reception"};
+const char* kMessage = {"Message"};
+const char* kChoose = {"Write number (n), write value (v) or send data (s)!"};
+const char* kInputNumber = {"Write number of the light!"};
+const char* kInputValue = {"Write intensity or command!"};
+const char* kSending = {"Sending Bytes: "};
 
-typedef struct Point {
-  uint8_t column;
-  uint8_t line;
-};
+const char* kCursorBackward = {"\0x1B"};
+const char* kCursorDown = {"\0x18"};
+const char* kCursorForward = {"\0x18"};
+const char* kCursorPosition = {"\0x18"};
+const char* kCursorUp = {"\0x18"};
 
-typedef struct Line {
-  struct Point;
-  uint8_t length;
-};
+const uint8_t kBufferSizeCommand = 16;
+const uint8_t kBufferSizeNumber = 4;
+const uint8_t kBufferSizeValue = 4;
+const uint8_t kLineTop = 1;
+const uint8_t kLineBottom = 24;
+const uint8_t kColumnLeft = 1;
+const uint8_t kColumnRight = 80;
 
-typedef struct Rectangle {
-  struct Point;
-  uint8_t colums;
-  uint8_t lines;
-};
+const char* kCursorHome = {"\27[H"};
+const char* kClearScreen = {"\27[2J"};
+
+const uint8_t kColumnLabel = 4;
+const uint8_t kColumnValue = 16;
+const uint8_t kLineCaption = 3;
+const uint8_t kLineNumber = 7;
+const uint8_t kLineValue = 10;
+const uint8_t kLineReception = 13;
+const uint8_t kLineMessage = 20;
+
+void DrawHorizontalLine(struct TuiFsm*);
+void DrawVerticalLine(struct TuiFsm*);
+void MoveCursorHome(struct TuiFsm*);
 
 void DrawCorner(void);
 void DrawHorizontalLineFromLeft2Right(uint8_t);
 void DrawHorizontalLineFromRight2Left(uint8_t);
-void DrawBlankRectangle();
 void DrawRectangle(uint8_t, uint8_t);
 void DrawVerticalLineFromBottom2Top(uint8_t);
 void DrawVerticalLineFromTop2Bottom(uint8_t);
@@ -46,8 +61,45 @@ void MoveCursorDown(void);
 void MoveCursorForward(void);
 void MoveCursorBackward(void);
 void MoveCursorUp(void);
-void PrintBottomOrUpperLine(uint8_t);
-void PrintMiddleLine(uint8_t);
 void SetCursor(uint8_t, uint8_t);
+
+typedef enum {
+  CHOOSE_ACTION,
+  READ_NUMBER,
+  READ_VALUE,
+  RECEIVE,
+  SEND
+} State;
+
+struct TuiFsm* {
+  State state;
+  unsigned char buffer_number[3];
+  unsigned char buffer_value[3];
+  unsigned char buffer_command[16];
+  uint8_t number;
+  uint8_t value;
+} tui_fsm;
+
+void InitTuiFsm(struct TuiFsm* tui_fsm);
+
+void ShowDaliAnswer(struct TuiFsm* tui_fsm, uint8_t answer);
+
+void InitMessage(struct TuiFsm* tui_fsm);
+
+void InitReadNumber(struct TuiFsm* tui_fsm);
+
+void InitReadValue(struct TuiFsm* tui_fsm);
+
+void MoveCursorHome(struct TuiFsm* tui_fsm);
+
+void MoveCursorToPosition(uint8_t line, uint8_t column, struct TuiFsm* puffer);
+
+void ClearScreen(struct TuiFsm* puffer);
+
+void ClearLine(struct TuiFsm* puffer);
+
+void ClearBuffer(unsigned char * buffer, unsigned char size_of_buffer);
+
+void HandleChar(char input, struct TuiFsm* tui_fsm);
 
 #endif // DALIINTERFACE_LIGHT_TUI
